b47bcd4608fb9307cf0b308862a882797874ccf9
Danilocick <daniel.hdez.11.31@gmail.com>
2023-11-10 19:45:09 +0100

    front done

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,18 +1,12 @@
 import React from "react";
 
 import "./../css/app.css";
-import Logo from "./../images/logo.jpg";
+import Logo from "./../images/logo.png";
 
 function Title(props) {
     return(
         <div class="header">
-            <div className="third">
-                <img className="logo" src={Logo} alt="Logo"/>
-            </div>
-            <div className="two-third">
-                <h2>CLUB BASQUET</h2>
-                <h2>SANTA COLOMA</h2>
-            </div>
+            <img src={Logo} alt="Logo"/>
         </div>
     );
 }
